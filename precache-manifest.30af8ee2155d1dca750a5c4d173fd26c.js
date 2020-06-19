28544ad64e91dc8467bc94cbbe796072ce7bcf01
Ymaril <martinsep@yandex.ru>
2020-06-19 21:32:33 +0300

    Updates

---
+++ precache-manifest.30af8ee2155d1dca750a5c4d173fd26c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ef4271d3f9a24b3d277eff3fc83b37e",
+    "url": "/order-component/index.html"
+  },
+  {
+    "revision": "dc061940386814a12011",
+    "url": "/order-component/static/css/main.cef29252.chunk.css"
+  },
+  {
+    "revision": "165caf48eacf5b9dec81",
+    "url": "/order-component/static/js/2.75e29a0b.chunk.js"
+  },
+  {
+    "revision": "25344171b2177ddcda5aba8d823f9930",
+    "url": "/order-component/static/js/2.75e29a0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc061940386814a12011",
+    "url": "/order-component/static/js/main.6ce84e38.chunk.js"
+  },
+  {
+    "revision": "ba677645910b9e2888f5",
+    "url": "/order-component/static/js/runtime-main.004f650f.js"
+  },
+  {
+    "revision": "0c92d379abfa81cb4994bce616110e53",
+    "url": "/order-component/static/media/marker.0c92d379.svg"
+  }
+]);
